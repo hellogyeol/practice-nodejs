8983cd4a92ab34434f915871d60fa5c9e4ff1492
hellogyeol <hellogyeol@gmail.com>
2022-12-20 22:59:02 +0900

    test: POST 요청 값 확인해보기

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(req.body)
+  res.sendFile(`${__dirname}/index.html`)
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.sendFile(`${__dirname}/index.html`)
 })
 
 app.listen(port, () => {
