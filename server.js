1393e2db33fcb8412dbcf300a5699fb827592c65
hellogyeol <hellogyeol@gmail.com>
2022-12-24 22:00:10 +0900

    test: 초기 세팅으로 복귀

--- server.js
+++ server.js
@@ -2,24 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  console.log(req.body)
-  res.sendFile(`${__dirname}/index.html`)
-})
-
-app.post('/', (req, res) => {
-  console.log(req.body.todo)
   // res.sendFile(`${__dirname}/index.html`)
-  res.render(`${__dirname}/form.ejs`, {'todo':req.body.todo})
-})
-
-app.get('/test', (req, res) => {
-  console.log(req.body)
-  res.sendFile(`${__dirname}/ejs-test.html`)
+  res.send('success')
 })
 
 app.listen(port, () => {
