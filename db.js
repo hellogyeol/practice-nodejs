47ec3b45387746f361dd709f8ea0d9d9b7f65896
hellogyeol <hellogyeol@gmail.com>
2022-12-31 23:21:26 +0900

    test: 데이터 삽입 테스트

--- db.js
+++ db.js
@@ -2,18 +2,25 @@ const { MongoClient } = require("mongodb");
                                                                                                                                      
 const url = "mongodb+srv://hellogyeol:hihello@cluster0.pnnqiq8.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
+const dbName = 'test'
 
 async function run() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
 
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
+    const db = client.db(dbName)
+    await db.collection('todo').insertOne({content: 'sleep'})
+
+    const list = db.collection('todo').find({})
+    console.log(list)
+  }
+  catch (err) {
+    console.log(err.stack);
+  }
+  finally {
+    await client.close();
+  }
 }
 
 run().catch(console.dir);
