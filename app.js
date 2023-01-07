c95a834c0617f7b98d34bf0247e0355173a565e4
hellogyeol <hellogyeol@gmail.com>
2023-01-08 00:23:12 +0900

    refactor: GET 요청 코드 재작성

--- app.js
+++ app.js
@@ -12,40 +12,21 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+  async function getTest() {
+    await client.connect();
+    const list = client.db('todoApp').collection('list');
+  
+    const cursor = list.find({});
+  
+    await cursor.forEach(console.log);
+    await client.close();
+
+    res.render('index');
+  }
   getTest();
-  res.render('index');
 });
 
-app.post('/', (req, res) => {
-  const content = req.body.content;
-  postTest(content);
-  res.render('index');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
-async function getTest() {
-  await client.connect();
-
-  const list = client.db('todoApp').collection('list');
-  const cursor = list.find({});
-
-  await cursor.forEach(console.log);
-  await client.close();
-}
-
-async function postTest(content) {
-  await client.connect();
-
-  const list = client.db('todoApp').collection('list');
-  await list.insertOne({
-    content: content
-  });
-  const cursor = list.find({});
-
-  await cursor.forEach(console.log);
-  await client.close();
-}
