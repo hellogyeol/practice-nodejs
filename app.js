da862c54df2f7c8596ce2f878b899029c72e1230
hellogyeol <hellogyeol@gmail.com>
2023-01-10 23:13:23 +0900

    test: CommonJS, ES6 모듈 import 테스트

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express');
+import path from 'path';
+const __dirname = path.resolve();
+// const express = require('express');
+import express from 'express'
 const app = express();
 const port = 3000;
 
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
+import { MongoClient } from 'mongodb';
 const url = "mongodb+srv://hellogyeol:hihello@cluster0.pnnqiq8.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
@@ -27,34 +31,17 @@ app.get('/', (req, res) => {
   getTest();
 });
 
-app.post('/', (req, res) => {
-  const content = req.body.content;
-  
-  async function postTest() {
-    await client.connect();
-    const col = client.db('db').collection('col');
 
-    await col.insertOne({
-      content: content
-    });
-    const todoList = await col.find({}).toArray();
-    console.log(todoList);
-    await client.close();
 
-    res.render('index', {
-      todoList: todoList
-    });
-  }
-  postTest();
-});
 
 app.get('/test', (req, res) => {
+  // res.sendFile('/Users/HANGYEOL/Documents/dev/practice/practice-nodejs/test.html')
   res.sendFile(`${__dirname}/test.html`)
 })
 
-app.get('/test2', (req, res) => {
-  res.send('wow')
-})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- public/index.js
+++ public/index.js
@@ -1,10 +1,19 @@
-console.log('test')
-const h3 = document.querySelector('.h3')
-h3.style.color = 'red'
+// const { MongoClient } = require("mongodb");
+// import { MongoClient } from "mongodb";
+// const url = "mongodb+srv://hellogyeol:hihello@cluster0.pnnqiq8.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(url);
 
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', async () => {
-  let res = await fetch('http://localhost:3000/test2')
-  let wow = await res.text()
-  console.log(wow)
-})
+
+// const btn = document.querySelector('.btn')
+// btn.addEventListener('click', getTest)
+
+// async function getTest() {
+//   await client.connect();
+//   const col = client.db('db').collection('col');
+
+//   const todoList = await col.find({}).toArray();
+//   console.log(todoList);
+// }
+
+import { MongoClient } from "../node_modules/mongodb/"
+console.log('test')
