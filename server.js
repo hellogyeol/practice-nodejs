4a0e5b468c8d3e8147adf5299686a90111b28b08
hellogyeol <hellogyeol@gmail.com>
2023-01-12 23:39:14 +0900

    test: SSR, CSR 코드 정리

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 const { MongoClient } = require("mongodb");
-const url = process.env.URL;
+const url = process.env.DB_URL;
 const client = new MongoClient(url);
 
 app.use(express.json());
@@ -16,39 +16,36 @@ app.get('/', (req, res) => {
   async function getTest() {
     await client.connect();
     const col = client.db('db').collection('col');
-
-    const todoList = await col.find({}).toArray();
-    console.log(todoList);
+  
+    const fruits = await col.find({}).toArray();
+    console.log(fruits);
+  
     await client.close();
-
+  
     res.render('index', {
-      todoList: todoList
+      fruits: fruits
     });
   }
   getTest();
 });
 
-
-app.get('/test', (req, res) => {
-  res.sendFile(`${__dirname}/test.html`)
+app.get('/csr', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
 });
 
-app.get('/test/test', (req, res) => {
+app.get('/csr/get', (req, res) => {
   async function getTest() {
     await client.connect();
     const col = client.db('db').collection('col');
-
-    const todoList = await col.find({}).toArray();
-    console.log(todoList);
+  
+    const fruits = await col.find({}).toArray();
+    console.log(fruits);
+  
     await client.close();
-
-    res.send(todoList)
+    res.send(fruits);
   }
-  getTest()
-  // res.sendFile(`${__dirname}/test.html`)
+  getTest();
 });
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
