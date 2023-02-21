2c49d2cfa61237a4e0d56e8b251df0c334ed0ce6
hellogyeol <hellogyeol@gmail.com>
2023-02-21 22:58:33 +0900

    test: form, pug 동적변수

--- server.js
+++ server.js
@@ -3,93 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const { MongoClient } = require("mongodb");
-const url = process.env.DB_URL;
-const client = new MongoClient(url);
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
-  getTest();
-  async function getTest() {
-    await client.connect();
-    const col = client.db('db').collection('col');
-  
-    const fruits = await col.find({}).toArray();
-    console.log(fruits);
-  
-    await client.close();
-  
-    res.render('index', {
-      fruits: fruits
-    });
-  }
+  res.render('index');
 });
 
-// app.post('/', (req, res) => {
-//   postTest();
-//   async function postTest() {
-//     const content = req.body.content;
-
-//     await client.connect();
-//     const col = client.db('db').collection('col');
-  
-//     await col.insertOne({fruit: content});
-//     const fruits = await col.find({}).toArray();
-//     console.log(fruits);
-  
-//     await client.close();
-
-//     res.render('index', {
-//       fruits: fruits
-//     });
-//   }
-// });
-
-
-/////////////////////////////////////////////////////////////////
-app.get('/csr', (req, res, next) => {
-  getTest();
-  async function getTest() {
-    await client.connect();
-    const col = client.db('db').collection('col');
-  
-    const fruits = await col.find({}).toArray();
-    console.log(fruits);
-  
-    await client.close();
-
-    res.send(fruits);
-    next();
-  }
-}, (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.post('/', (req, res) => {
+  res.render('index', {
+    str: req.body.val
+  });
 });
 
-// app.post('/csr', (req, res) => {
-//   postTest();
-//   async function postTest() {
-//     const content = req.body.content;
-
-//     await client.connect();
-//     const col = client.db('db').collection('col');
-  
-//     await col.insertOne({fruit: content});
-//     const fruits = await col.find({}).toArray();
-//     console.log(fruits);
-  
-//     await client.close();
-
-//     res.send(fruits);
-//   }
-// });
-
-
-//////////////////////////////////////////////////////////////////
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
